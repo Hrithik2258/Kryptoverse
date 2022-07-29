598aafe6453bcabdadb3ebf664c409c57c96e25f
Hrihtik Pawar <86373306+Hrithik2258@users.noreply.github.com>
2022-07-29 18:57:15 +0530

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+// import abi from "./Transactions.json";
+
+// export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+// export const contractABI = abi.abi;
+
+
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+
+export const contractAddress = '0x9f180E957Dc8D886c38BE418BCEbef2a5ec12585'
